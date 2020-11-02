49dbf3999b92c85eb4197d6ee905b4b6557d5479
zoheir <zoheirmaz.zm@gmail.com>
2020-11-02 21:57:46 +0330

    feat: add helper method to do payment

--- app/helpers.php
+++ app/helpers.php
@@ -1,1 +1,32 @@
 <?php
+
+use App\Entities\Transaction;
+use Shetabit\Multipay\Invoice;
+use Shetabit\Payment\Facade\Payment;
+use Illuminate\Support\Facades\Auth;
+use Shetabit\Multipay\Abstracts\Driver;
+use App\Entities\Payment as PaymentEntity;
+use Infrastructure\Enums\PaymentGatewaysEnums;
+use Infrastructure\Enums\TransactionStatusEnums;
+
+if (!function_exists('pay')) {
+    function pay(PaymentEntity $payment)
+    {
+        return Payment::purchase(
+            (new Invoice)->amount(1000)->detail(
+                'mobile',
+//                Auth::user()->{User::MOBILE}
+                '09195824409'
+            ),
+            function (Driver $driver, $transactionId) use ($payment) {
+                Transaction::create([
+                    Transaction::CREATED_BY => Auth::id(),
+                    Transaction::TRANSACTION_ID => $transactionId,
+                    Transaction::GATEWAY => PaymentGatewaysEnums::ZARINPAL,
+                    Transaction::STATUS => TransactionStatusEnums::WAITING,
+                    Transaction::PAYMENT_ID => $payment->{PaymentEntity::ID},
+                ]);
+            }
+        )->pay()->toJson();
+    }
+}
